4b92a5d9415a79d3ada031a2b01925a07fe4829d
zhushuhang <zhushuhang@onemt.com.cn>
2018-07-05 12:21:17 +0800

    code optimization

--- app/src/main/java/com/sc/github/view/FlashView.java
+++ app/src/main/java/com/sc/github/view/FlashView.java
@@ -37,12 +37,10 @@ public class FlashView extends View {
 
     private Paint mDstPaint;
     private Paint mSrcPaint;
-    private Bitmap mDstBmp;
-    private RectF mDstRect;
-    private int mFlashDuration;
+    private Bitmap mSrcBitmap;
     private Bitmap mMaskBitmap;
     private Bitmap mRenderMaskBitmap;
-    private Canvas mMaskCanvas;
+    private Canvas mRenderMaskBitmapCanvas;
     private int mMaskWidth;
     private int mMaskHeight;
     private float mHorMoveDistance;
@@ -51,6 +49,7 @@ public class FlashView extends View {
     private int mStartAlpha = 130;
     private ObjectAnimator mAnimator;
     private boolean mPlaying = false;
+    private int mFlashDuration;
 
     public FlashView(@NonNull Context context) {
         this(context, null);
@@ -62,22 +61,22 @@ public class FlashView extends View {
 
     public FlashView(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mFlashDuration = DEFAULT_DURATION;
+        mSrcPaint = new Paint();
+        mSrcPaint.setAlpha(mStartAlpha);
         Xfermode mode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
         mDstPaint = new Paint();
         mDstPaint.setAntiAlias(true);
         mDstPaint.setDither(true);
         mDstPaint.setFilterBitmap(true);
         mDstPaint.setXfermode(mode);
-        mSrcPaint = new Paint();
-        mSrcPaint.setAlpha(mStartAlpha);
-        mFlashDuration = DEFAULT_DURATION;
-        Resources resources = getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
         mMaskWidth = ((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_MASK_WIDTH, metrics));
         mMaskHeight = ((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_MASK_HEIGHT, metrics));
-        mMaskBitmap = createMaskBitmap();
-        mRenderMaskBitmap = createRenderMaskBitmap();
-        mMaskCanvas = new Canvas(mRenderMaskBitmap);
     }
 
     @Override
@@ -89,28 +88,36 @@ public class FlashView extends View {
         int resultW = widthSize;
         int resultH = heightSize;
         if (widthMode == MeasureSpec.AT_MOST) {
-            resultW = mDstBmp.getWidth();
+            resultW = mSrcBitmap.getWidth();
         }
         if (heightMode == MeasureSpec.AT_MOST) {
-            resultH = mDstBmp.getHeight();
+            resultH = mSrcBitmap.getHeight();
         }
         setMeasuredDimension(resultW, resultH);
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int ow, int oh) {
-        super.onSizeChanged(w, h, ow, oh);
-        mDstRect = new RectF(0, 0, w, h);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(mDstBmp, 0f, 0f, mSrcPaint);
-        mMaskCanvas.drawColor(0, PorterDuff.Mode.CLEAR);
-        mMaskCanvas.drawBitmap(mDstBmp, null, mDstRect, null);
-        mMaskCanvas.drawBitmap(mMaskBitmap, mHorMoveDistance, 0, mDstPaint);
+        ensureSrcBitmapNotNull();
+        // 绘制源图
+        drawSrcOnCanvas(canvas, mSrcPaint);
+        // 在遮罩图上绘制源图
+        ensureRenderMaskBitmapNotNull();
+        drawSrcOnCanvas(mRenderMaskBitmapCanvas, null);
+        // 在遮罩图上绘制渐变遮罩
+        ensureMaskBitmapNotNull();
+        mRenderMaskBitmapCanvas.drawBitmap(mMaskBitmap, mHorMoveDistance, 0, mDstPaint);
+        // 最终映射到控件上
         canvas.drawBitmap(mRenderMaskBitmap, 0, 0, null);
+    }
+
+    private void drawSrcOnCanvas(Canvas canvas, Paint paint) {
+        canvas.drawBitmap(mSrcBitmap, 0f, 0f, paint);
+    }
+
+    private void clearCanvas(Canvas canvas) {
+        canvas.drawColor(0, PorterDuff.Mode.CLEAR);
     }
 
     private void setPercent(float percent) {
@@ -119,23 +126,33 @@ public class FlashView extends View {
     }
 
     public void setImage(int resId) {
-        mDstBmp = BitmapFactory.decodeResource(getResources(), resId);
+        mSrcBitmap = BitmapFactory.decodeResource(getResources(), resId);
     }
 
-    private Bitmap createMaskBitmap() {
-        int horMoveArea = ((int) (this.mMaskWidth * 2.5f));
-        int verMoveArea = this.mMaskHeight;
-        Shader shader = new LinearGradient(0.35f * horMoveArea, 1.0f * verMoveArea, 0.65f * horMoveArea, 0.0f * verMoveArea, this.mStartColor, this.mEndColor, Shader.TileMode.CLAMP);
-        Bitmap maskBitmap = Bitmap.createBitmap(horMoveArea, verMoveArea, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(maskBitmap);
-        Paint paint = new Paint();
-        paint.setShader(shader);
-        canvas.drawRect(0.0f, 0.0f, horMoveArea, verMoveArea, paint);
-        return maskBitmap;
+    private void ensureSrcBitmapNotNull() {
+        if (mSrcBitmap == null) {
+            throw new RuntimeException("You need call setImage(int resId) to set a src image!");
+        }
     }
 
-    private Bitmap createRenderMaskBitmap() {
-        return Bitmap.createBitmap(mMaskWidth, mMaskHeight, Bitmap.Config.ARGB_8888);
+    private void ensureRenderMaskBitmapNotNull() {
+        if (mRenderMaskBitmap == null) {
+            mRenderMaskBitmap = Bitmap.createBitmap(mMaskWidth, mMaskHeight, Bitmap.Config.ARGB_8888);
+            mRenderMaskBitmapCanvas = new Canvas(mRenderMaskBitmap);
+        }
+    }
+
+    private void ensureMaskBitmapNotNull() {
+        if (mMaskBitmap == null) {
+            int horMoveArea = ((int) (this.mMaskWidth * 2.5f));
+            int verMoveArea = this.mMaskHeight;
+            Shader shader = new LinearGradient(0.35f * horMoveArea, 1.0f * verMoveArea, 0.65f * horMoveArea, 0.0f * verMoveArea, this.mStartColor, this.mEndColor, Shader.TileMode.CLAMP);
+            mMaskBitmap = Bitmap.createBitmap(horMoveArea, verMoveArea, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(mMaskBitmap);
+            Paint paint = new Paint();
+            paint.setShader(shader);
+            canvas.drawRect(0.0f, 0.0f, horMoveArea, verMoveArea, paint);
+        }
     }
 
     public void start() {
